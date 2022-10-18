d79348189f88b2f1f6ebbea5cd7178bf7156b8af
Sophie Hogg <sophiehogg10@gmail.com>
2022-10-18 15:26:22 +1100

    fixed visuals, completed MVP

--- README.md
+++ README.md
@@ -1,29 +1,37 @@
-# Poker Practice
+# Poker Practicing App
 
-A 1 person poker game - getting used to card mechanics, graphics, and randomness in JavaScript. This was made as a personal project using JavaScript, HTML and SCSS.
+A 1 person poker game - getting used to card mechanics, graphics, and randomness in JavaScript. This is a personal project using JavaScript, HTML and SCSS.
+
+![image of poker website](./images/screenshot-poker-readme.PNG)
 
 ## MVP
 
 -   A system that deals two cards to a player, then deals cards to the centre in the same manner as in poker
 -   Ability to fold
--   Ability to see remaining cards
+-   Ability to see remaining cards on fold or on check
 -   Ability to redeal
+-   Different coloured cards depending on the suit
+-   Suit images
 
 ## Palette and Design
 
 Many poker games online use the same green and red palette, so I decided to go against this and opted for a coral palette generated at coolors.co
-#F7B267
-#F79D65
-#F4845F
-#F27059
-#F25C54
 
 In order to allow for this, I made the design for the card back on onlinefractaltools.com to imitate the complex designs that commonly appear on the backs of cards.
 
 In addition, I opted for some darker colours to counter the more piercing effect of these colours, especially darker reddish-browns and purples.
 
-## Extensions:
+## Updates: 18/10
 
--   Ability to bet
--   Tracking player money
--   Tracking player win rate?
+-   Added ability to bet, track player money and pot.
+-   Added ability to restart and reset player money
+-   Refactored code to reduce repetition
+-   Fixed visuals
+-   Added favicon
+
+### To Do
+
+-   Add an 'are you sure' functionality to restart to ensure no accidental restarts
+-   Figuring out what hand the player achieved
+-   Enable player to set starting money amount
+-   Blinds set on a timer
--- script.js
+++ script.js
@@ -6,15 +6,27 @@
 let fold = document.getElementById("fold");
 let check = document.getElementById("check");
 let redeal = document.getElementById("redeal");
+let restart = document.getElementById("restart");
+let submit = document.getElementById("submit");
+let popup = document.getElementById("popup");
+let money = document.getElementById("money");
+let pot = document.getElementById("pot");
 
 var suits = ["hearts", "clubs", "diamonds", "spades"];
 var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+var playerMoney = 1000;
+var potMoney = 0;
+
+money.innerHTML = playerMoney;
+pot.innerHTML = potMoney;
 
 let flopOcc = false;
 let turnOcc = false;
 let riverOcc = false;
 let dealOcc = false;
 
+//create deck by looping through and adding every value to each suit and pushing these 'card' elements to an array.
 function getDeck() {
     let deck = [];
     for (let i = 0; i < suits.length; i++) {
@@ -25,10 +37,10 @@ function getDeck() {
     }
     return deck;
 }
+
 var deck1 = getDeck();
-shuffle(deck1);
-//now that the deck is created, need to shuffle it by randomising the order.
-//this code represents the Durstenfeld shuffle and is based on the Wikipedia article
+
+//now that the deck is created, need to shuffle it by randomising the order. This code represents the Durstenfeld shuffle (code adapted from Wikipedia)
 function shuffle(deck) {
     for (let i = deck.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1)); // returns a random index (for a random card) between 0 and i
@@ -36,58 +48,80 @@ function shuffle(deck) {
     }
 }
 
-function drawCard(card) {
-    let cardCol = "black";
-    if (card.suit == "diamonds" || card.suit == "hearts") {
-        cardCol = "red";
-    }
+shuffle(deck1);
+
+//now need to create the card as an HTML element based on the array element. This function also determines whether the card goes to the table or to the player hand (in future, may also go to opponent hand).
+function createCard(destination, card) {
     const newCard = document.createElement("div");
     newCard.classList.add("poker__card");
-    var text = document.createTextNode(`${card.value} ${card.suit}`);
+    var text = document.createTextNode(`${card.value}`);
     newCard.appendChild(text);
-    newCard.style.color = cardCol;
-    pokerhand.appendChild(newCard);
-}
-function placeCard(card) {
-    let cardCol = "black";
-    if (card.suit == "diamonds" || card.suit == "hearts") {
-        cardCol = "red";
-    }
-    const newCard = document.createElement("div");
-    newCard.classList.add("poker__card");
-    var text = document.createTextNode(`${card.value} ${card.suit}`);
-    newCard.appendChild(text);
-    newCard.style.color = cardCol;
-    tabled.appendChild(newCard);
+    addSuit(newCard, card.suit);
+    destination.appendChild(newCard);
 }
 
+//this function adds the correct suit image to the card, and also defines the card colour (black or red)
+function addSuit(newCard, suit) {
+    var cardCol = "black";
+    var suitImg = document.createElement("img");
+    suitImg.classList.add("poker__card__suitImg");
+    var cardSuit = document.createElement("div");
+    cardSuit.classList.add("poker__card__cardSuit");
+    switch (suit) {
+        case "hearts":
+            suitImg.src = "images/card-heart.png";
+            cardCol = "red";
+            break;
+        case "diamonds":
+            suitImg.src = "images/card-diamond.png";
+            cardCol = "red";
+            break;
+        case "spades":
+            suitImg.src = "images/card-spade.png";
+            break;
+        case "clubs":
+            suitImg.src = "images/card-club.png";
+            break;
+        // if something goes wrong and none of the card suits are found, a joker will display
+        default:
+            suitImg.src = "images/card-joker.png";
+            break;
+    }
+    cardSuit.appendChild(suitImg);
+    newCard.appendChild(cardSuit);
+    newCard.style.color = cardCol;
+}
+
+//this deals out cards to the user.
 function deal() {
-    console.log(deck1[0]);
-    console.log(deck1[1]);
-    drawCard(deck1[0]);
-    drawCard(deck1[1]);
+    createCard(pokerhand, deck1[0]);
+    createCard(pokerhand, deck1[1]);
     dealOcc = true;
 }
 
+//this deals the first cards to the table
 function flop() {
-    placeCard(deck1[2]);
-    placeCard(deck1[3]);
-    placeCard(deck1[4]);
+    createCard(tabled, deck1[2]);
+    createCard(tabled, deck1[3]);
+    createCard(tabled, deck1[4]);
     flopOcc = true;
 }
 
+//this deals the fourth card to the table
 function turn() {
-    placeCard(deck1[5]);
+    createCard(tabled, deck1[5]);
     turnOcc = true;
 }
 
+//this deals the fifth card to the table
 function river() {
-    placeCard(deck1[6]);
+    createCard(tabled, deck1[6]);
     riverOcc = true;
 }
 
 deal();
 
+//this occurs if the player selects fold: reveals all remaining cards
 fold.addEventListener("click", () => {
     if (!flopOcc) {
         flop();
@@ -102,7 +136,8 @@ fold.addEventListener("click", () => {
     //You achieved a ... - extension task
 });
 
-check.addEventListener("click", () => {
+//since this is used twice (in the check event Listener and the bidding submission event listener)
+function newCard() {
     if (!flopOcc) {
         flop();
     } else if (!turnOcc) {
@@ -110,9 +145,15 @@ check.addEventListener("click", () => {
     } else if (!riverOcc) {
         river();
     }
+}
+
+//if the player clicks check, the next set of cards will display
+check.addEventListener("click", () => {
+    newCard();
     // else{"You achieved a ... - extension task"}
 });
 
+//if the player hits redeal at any time, all tabled cards will be removed and the player will be dealt two new cards from the shuffled deck
 redeal.addEventListener("click", () => {
     tabled.innerHTML = "";
     pokerhand.innerHTML = "";
@@ -120,5 +161,39 @@ redeal.addEventListener("click", () => {
     turnOcc = false;
     riverOcc = false;
     dealOcc = false;
+    potAmount = 0;
+    shuffle(deck1);
     deal();
+});
+
+restart.addEventListener("click", () => {
+    tabled.innerHTML = "";
+    pokerhand.innerHTML = "";
+    flopOcc = false;
+    turnOcc = false;
+    riverOcc = false;
+    dealOcc = false;
+    shuffle(deck1);
+    deal();
+    playerMoney = 1000;
+    potMoney = 0;
+    money.innerHTML = playerMoney;
+    pot.innerHTML = potMoney;
+});
+
+//if the player clicks the bid button, they have the option to type in any number. This is the value they wish the bid for.
+function showBox() {
+    popup.classList.toggle("poker__buttons__text__show");
+    submit.classList.toggle("poker__buttons__submit__show");
+}
+
+//they must then be able to submit this bid. This should change the amount in the pot, the amount they have remaining, and should automatically draw the next card.
+
+submit.addEventListener("click", () => {
+    let bidAmount = parseInt(popup.value);
+    playerMoney = playerMoney - bidAmount;
+    money.innerHTML = playerMoney;
+    potMoney = potMoney + bidAmount;
+    pot.innerHTML = potMoney;
+    newCard();
 });
